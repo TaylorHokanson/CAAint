900d090d511e289e51cf716eccf49a384a5a7204
Taylor Hokanson <taylor@taylorhokanson.com>
2018-05-02 11:50:35 -0500

    Update master.ino

--- Modules/i2c_test/i2c_string/master.ino
+++ Modules/i2c_test/i2c_string/master.ino
@@ -1,7 +1,11 @@
-// Tested on UNO
-
-// Based on Wire Master Reader
-// by Nicholas Zambetti <http://www.zambetti.com>
+/* Tested on the Arduion UNO
+ * Connect SDA/SDL/3.3V/GND to Attiny85 running slave code
+ * Open Serial port to see results
+ * Don't forget to match up your slave device ID below
+ *
+ * based on Wire Master Reader
+ * by Nicholas Zambetti <http://www.zambetti.com>
+*/
 
 #include <Wire.h>
 
@@ -11,7 +15,7 @@ void setup() {
 }
 
 void loop() {
-  Wire.requestFrom(10, 2);    // request x bytes from slave device #y
+  Wire.requestFrom(10, 32);    // request 32 bytes from slave device #10
 
   while (Wire.available()) { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
