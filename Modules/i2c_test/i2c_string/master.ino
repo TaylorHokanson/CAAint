32b2dba0fd4d26a809ebca91b60b458950342a76
Taylor Hokanson <taylor@taylorhokanson.com>
2018-05-02 10:27:44 -0500

    Update master.ino

--- Modules/i2c_test/i2c_string/master.ino
+++ Modules/i2c_test/i2c_string/master.ino
@@ -1,14 +1,7 @@
-// Wire Master Reader
+// Tested on UNO
+
+// Based on Wire Master Reader
 // by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Reads data from an I2C/TWI slave device
-// Refer to the "Wire Slave Sender" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
 
 #include <Wire.h>
 
