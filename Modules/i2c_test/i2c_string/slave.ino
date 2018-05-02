03e12da6ed6e3269f047eaeb360fe06933c9af5f
Taylor Hokanson <taylor@taylorhokanson.com>
2018-05-02 11:47:00 -0500

    Update slave.ino

--- Modules/i2c_test/i2c_string/slave.ino
+++ Modules/i2c_test/i2c_string/slave.ino
@@ -1,13 +1,34 @@
+
+/*  Based on the slave_sender example in the TinyWire library
+ *  https://github.com/lucullusTheOnly/TinyWire
+ *  
+ *  Photoresistor leg 1 >> 3.3V
+ *  Photoresistor leg 2 >> 10k resistor >> GND
+ *  Photoresistor leg 2 >> Attiny A3
+ *  
+ *  Connect Attiny SDA and SCL to UNO SDA and SCL
+ *  Supply 3.3V and GND to Attiny from UNO
+ *  
+ *  Attiny85 pinout:     
+ *  
+ *       RST -|*  |- 3.3/5V
+ *      A3/3 -|   |- A1/2   <-- SCL
+ *      A2/4 -|   |- PWM/1
+ *       GND -|   |- 0/PWM  <-- SDA
+ */
+
 #include <TinyWire.h>
 
 byte own_address = 10;
-char charBuf[2] = {'h','i'};
+// can't use A1 with ic2?
+int analogInPin = A3;
 
 void setup() {
 	// config TinyWire library for I2C slave functionality
 	TinyWire.begin( own_address );
 	// register a handler function in case of a request from a master
 	TinyWire.onRequest( onI2CRequest );
+   pinMode(analogInPin, INPUT);
 }
 
 void loop() {
@@ -17,10 +38,27 @@ void loop() {
 // Request Event handler function
 //  --> Keep in mind, that this is executed in an interrupt service routine. It shouldn't take long to execute
 void onI2CRequest() {
-	// sends x bytes to the master, regardless how many bytes he expects
-	// if the buffer is empty, but the master is still requesting, the slave aborts the communication
-	// (so it is not blocking)
   
-  TinyWire.send(charBuf[0]);
-  TinyWire.send(charBuf[1]);
+  // take sensor reading and add descriptive text
+  int analogReading = analogRead(analogInPin);  
+  String contextText = "Sensor: ";
+  String sensorReading = contextText + analogReading;
+  
+  // ascertain the string length and convert to char array
+  char charBuf[sensorReading.length()+1];
+  sensorReading.toCharArray(charBuf, sensorReading.length()+1); 
+  
+  // send the character array to master
+  for (int i = 0; i < sizeof(charBuf) - 1; i++){
+    TinyWire.send(charBuf[i]);
+  }
+
+  // fill the rest of the 32 byte request with neutral chars and send
+  int bytesLeft = 32 - sizeof(charBuf);
+  for (int i = 0; i < bytesLeft; i++){
+    TinyWire.send('.');
+  }
+
+  sensorReading = "";
+  
 }
